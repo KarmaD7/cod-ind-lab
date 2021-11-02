ddba3fc0e9e7b9c6123fe23ec65140e2be8d3ad8
Shaofeng Ding <thudsf19@gmail.com>
2021-11-02 20:02:46 +0800

    asm bug fix

--- lab5.s
+++ lab5.s
@@ -4,15 +4,15 @@
 _start:
   addi t0, x0, 0
   addi t1, x0, 1
-  addi t2, x0, 6
+  addi t2, x0, 101
 L1:
   add t0, t0, t1
   addi t1, t1, 1
   bne t1, t2, L1
 output_mem:
-  addi t3, t3, 0x401
-  slli t3, t3, 21
-  addi t3, t3, 100
+  addi t3, t3, 1
+  slli t3, t3, 31
+  addi t3, t3, 0x100
   sw t0, 0(t3)
 output_serial:
   addi t3, x0, 1
